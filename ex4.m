78807ca5bf8f2e9f734306474782328764bffff2
tamir_Shilony <tamirshilony@gmail.com>
2020-05-19 16:35:55 +0300

    fix

--- ex4.m
+++
@@ -1,5 +0,0 @@
-myFiles = dir('..\DATA_DIR\**\*.edf');
-charToValidate = '\d*E[OC]';
-validateFileNames = cellfun('isempty',regexp({myFiles.name},charToValidate));
-paths = structfun('strcat',myFiles
-myData = cellfun('edfread',cell([myFiles.folder '\' myFiles.name]));

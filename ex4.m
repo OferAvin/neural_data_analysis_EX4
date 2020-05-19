69ac8d14d62ed77f4d200e7681aa607349c1526c
tamir_Shilony <tamirshilony@gmail.com>
2020-05-19 16:34:36 +0300

    add buildstruct

--- ex4.m
+++
@@ -1,4 +0,0 @@
-MyFiles = dir('..\DATA_DIR\**\*.edf');
-charToValidate = '\d*E[OC]';
-validateFileNames = cellfun('isempty',regexp({MyFiles.name},charToValidate));
-[MyData] = build_struct(MyFiles);

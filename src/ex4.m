c8b58725396f8630c4d076419502d7a8f083e836
Ofer Avin <oferavin32@gmail.com>
2020-05-20 10:42:51 +0300

    fixed conflict

--- src/ex4.m
+++ src/ex4.m
@@ -5,10 +5,7 @@ validateFileNames = cellfun('isempty',regexp({myFiles.name},charToValidate));
 clc;
 clear;
 
-MyFiles = dir('..\DATA_DIR\**\*.edf');
 cond = ["EC","EO"];
 numOfSub = length(MyFiles)/length(cond);
-charToValidate = '\d*E[OC]';
-validateFileNames = cellfun('isempty',regexp({MyFiles.name},charToValidate));
 MyData = BuildStruct(MyFiles,numOfSub,cond);
 

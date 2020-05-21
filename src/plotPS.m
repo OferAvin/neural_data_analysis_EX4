683c381445ee8fd2a4d1f75f76943dc08ce81f9f
Ofer Avin <oferavin32@gmail.com>
2020-05-21 16:32:19 +0300

    sad

--- src/plotPS.m
+++ src/plotPS.m
@@ -5,11 +5,13 @@ function plotPS(f,fft1,pwelch1,dft1,fft2,pwelch2,dft2)
     hold on;
     plot(fft2,'Color','r');
     hold off;
+    
     subplot(1,3,2);
     plot(f,pwelch1,'Color','b');
     hold on;
     plot(f,pwelch2,'Color','r');
     hold off;
+    
     subplot(1,3,3);
     plot(f,dft1(1:length(f)),'Color','b');
     hold on;

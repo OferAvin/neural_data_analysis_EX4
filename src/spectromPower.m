25970358d38677c14105ce226df8f2880dc64eda
Ofer Avin <oferavin32@gmail.com>
2020-05-21 15:09:33 +0300

    cahnged specPower to fftPS

--- src/spectromPower.m
+++
@@ -1,8 +0,0 @@
-function[spectorVecShift] =  fftPS(data,condition,curSubject,electrodeNum)
-        dataVec = data.(curSubject).(condition).record(electrodeNum,:);
-        spectromVecComplex = fft(dataVec);  
-        spectromVec = spectromVecComplex/norm(spectromVecComplex);
-        spectromVecNorm = abs(spectromVec).^2;
-        spectorVecShift = fftshift(spectromVecNorm);
-        
-end

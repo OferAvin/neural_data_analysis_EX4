c79e25dcd0db1ce6eea15cf7f7aa8abddd24d696
tamir_Shilony <tamirshilony@gmail.com>
2020-05-21 15:24:26 +0300

    dftPs function

--- src/spectromPower.m
+++ src/spectromPower.m
@@ -1,8 +1,8 @@
 function[spectorVecShift] =  spectromPower(data,condition,curSubject,electrodeNum)
-        dataVec = data.(curSubject).(condition).record(electrodeNum,:);
-        spectromVecComplex = fft(dataVec);  
-        spectromVec = spectromVecComplex/norm(spectromVecComplex);
-        spectromVecNorm = abs(spectromVec).^2;
-        spectorVecShift = fftshift(spectromVecNorm);
-        
+    dataVec = data.(curSubject).(condition).record(electrodeNum,:);
+    spectromVecComplex = fft(dataVec);  
+    spectromVec = spectromVecComplex/length(spectromVecComplex);
+    spectromVecNorm = abs(spectromVec).^2;
+    spectorVecShift = fftshift(spectromVecNorm);
+
 end
